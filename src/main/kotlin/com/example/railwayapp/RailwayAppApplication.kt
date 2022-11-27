14698b342b36ced76c79273002aa0ca5db5aa617
AmadeAli1 <aliamade29@gmail.com>
2022-11-27 13:07:06 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/railwayapp/RailwayAppApplication.kt
@@ -0,0 +1,28 @@
+package com.example.railwayapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class RailwayAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<RailwayAppApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/api")
+class RailwayController(private val environment: Environment) {
+
+    @GetMapping()
+    fun all(): List<String> {
+        val env = environment["google.url"]
+        return listOf("Amade", "Vasco", "Ali", "Google: $env")
+    }
+
+}
